1cd1d5d72c15143d3a0912aa80de9042228a8750
Leandro Arturi <lea.arturi@gmail.com>
2023-09-10 21:03:54 -0300

    Commit

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ git add .
 git commit -m "Commit"
 git push
 
-GOOS=linux GOARCH=amd64 go build -o bootstrap main.go
-zip lambda-handler.zip bootstrap  
+GOOS=linux GOARCH=amd64 go build -o main main.go
+zip lambda-handler.zip main  
 
 # chmod +x deploy.sh
 # ./deploy.sh
